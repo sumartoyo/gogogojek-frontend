490dbcdc84561aa4c5c0492bbc5abfd2d65cb80b
Dimas <dimas.gs@outlook.com>
2016-11-17 19:57:53 +0700

    add datepicker; implements input controller

--- js/controller.js
+++ js/controller.js
@@ -1,5 +1,51 @@
 'use strict';
 
 angular.module('app', []).controller('MainCtrl', function() {
-  this.hahaha = 'hehehe';
+  var vm = this;
+
+  vm.aggregation = 'sum';
+
+  vm.dateFrom = '2015-01-01';
+  vm.dateTo = '2016-12-31';
+
+  vm.location = {
+    all: true,
+    timur: false,
+    barat: false,
+    utara: false,
+    pusat: false,
+    selatan: false,
+    onChange: function() {
+      ['timur', 'barat', 'utara', 'pusat', 'selatan'].forEach(prov => vm.location[prov] = vm.location.all);
+      vm.location.all = false;
+    },
+  };
+
+  vm.day = {
+    all: true,
+    monday: false,
+    tuesday: false,
+    wednesday: false,
+    thursday: false,
+    friday: false,
+    saturday: false,
+    sunday: false,
+    onChange: function() {
+      ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'].forEach(day => vm.day[day] = vm.day.all);
+      vm.day.all = false;
+    },
+  };
+
+  vm.timeFrom = 0;
+  vm.timeTo = 24;
+
+  vm.refresh = function() {
+    console.log('get data...');
+    console.log('process...');
+    console.log('show viz...');
+  };
+
+  // init
+  vm.location.onChange();
+  vm.day.onChange();
 });
