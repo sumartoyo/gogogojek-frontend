4bb582c3b62dba4ddf7184b7e9f3b3e8cb73d572
Dimas <dimas.gs@outlook.com>
2016-12-01 20:22:30 +0700

    add hasil evaluasi

--- js/controller.js
+++ js/controller.js
@@ -1,134 +1,234 @@
-angular.module('app', [
-  'rzModule'
-]).controller('MainCtrl', [
-  '$http',
-  function($http)
-{
-  var vm = this;
+(function() {
+  var module = angular.module('app', [
+    'rzModule',
+    'chart.js',
+  ])
 
-  /* config */
-
-  var sqrtItemCount = 94;
-  var apiUrl = 'http://54.169.56.109:8888/api/points';
-
-  /* view model */
-
-  vm.aggregation = 'sum';
-
-  vm.date = {
-    from: '2015-01-01',
-    to: '2015-12-31',
-  };
-
-  vm.location = {
-    all: true,
-    timur: false,
-    barat: false,
-    utara: false,
-    pusat: false,
-    selatan: false,
-    onChange: function() {
-      ['timur', 'barat', 'utara', 'pusat', 'selatan'].forEach(prov => vm.location[prov] = vm.location.all);
-      vm.location.all = false;
-    },
-  };
-
-  vm.day = {
-    all: true,
-    monday: false,
-    tuesday: false,
-    wednesday: false,
-    thursday: false,
-    friday: false,
-    saturday: false,
-    sunday: false,
-    onChange: function() {
-      ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'].forEach(day => vm.day[day] = vm.day.all);
-      vm.day.all = false;
-    },
-  };
-
-  vm.time = {
-    from: 6,
-    to: 18,
-    options: {
-      floor: 6,
-      ceil: 18,
-      step: 1,
-      translate: function(value, _, label) {
-        switch (label) {
-          case 'model':
-            return '<b>From</b> '+(value < 10 ? '0'+value : value)+'.00';
-          case 'high':
-            return '<b>To</b> '+(value < 10 ? '0'+value : value)+'.00';
-          default:
-            return '';
-        }
+  module.config(['ChartJsProvider', function (ChartJsProvider) {
+    ChartJsProvider.setOptions({
+      chartColors: ['#5DA5DA', '#FAA43A', '#60BD68', '#F17CB0', '#B2912F', '#B276B2', '#DECF3F', '#F15854', '#4D4D4D'],
+    });
+    ChartJsProvider.setOptions('pie', {
+      title: {
+        display: true,
       },
-    },
-  };
+    });
+    ChartJsProvider.setOptions('bar', {
+      title: {
+        display: true,
+      },
+    });
+    ChartJsProvider.setOptions('line', {
+      legend: {
+        display: true,
+      },
+      title: {
+        display: true,
+      },
+    });
+  }]);
 
-  vm.labelSubmit = 'Submit';
-  vm.errorMessage = '';
+  module.controller('MainCtrl', ['$http', '$timeout', '$scope', function($http, $timeout, $scope) {
+    var vm = this;
 
-  /* vars */
+    /* config */
 
-  var onEvent = function(trigger) {
-    vm.labelSubmit = 'Loading...';
-    console.log(trigger+' fired');
-  }
+    var sqrtItemCount = 94;
+    var apiUrl = 'http://54.169.56.109:8888/api/points';
 
-  var lastRequest = 0;
-  var payload = {
-    lat_from: -6.2303955,
-    long_from: 106.8480445,
-    lat_to: -6.0303955,
-    long_to: 107.0480445,
-    time_from: 10,
-    time_to: 18,
-    n_items: sqrtItemCount * sqrtItemCount,
-  };
-  var onChangeBounds = function() {
+    /* view model */
+
+    vm.viewType = 'evaluasi';
+    vm.status = 'all';
+    vm.city = 'all';
+
+    vm.labelSubmit = 'Submit';
     vm.errorMessage = '';
 
-    var bounds = maps.map.getBounds();
-    payload.lat_from = bounds.f.f;
-    payload.long_from = bounds.b.b;
-    payload.lat_to = bounds.f.b;
-    payload.long_to = bounds.b.f;
-    payload.time_from = vm.time.from;
-    payload.time_to = vm.time.to;
+    vm.legend = {
+      min: 100,
+      mid: 2000,
+      max: 40000,
+    }
 
-    var myRequest = (new Date).getTime();
-    lastRequest = myRequest;
-    console.log('requesting...', myRequest);
+    vm.time = {
+      from: 10,
+      to: 18,
+      options: {
+        floor: 10,
+        ceil: 18,
+        step: 1,
+        translate: function(value, _, label) {
+          switch (label) {
+            case 'model':
+              return '<b>From</b> '+(value < 10 ? '0'+value : value)+'.00';
+            case 'high':
+              return '<b>To</b> '+(value < 10 ? '0'+value : value)+'.00';
+            default:
+              return '';
+          }
+        },
+      },
+    };
 
-    $http.post(apiUrl, payload).then(function(res) {
-      if (res.status == 200) {
-        if (myRequest == lastRequest) {
-          maps.draw(res.data.data.points, sqrtItemCount);
-          vm.labelSubmit = 'Submit';
+    vm.chart = {
+      pie: {
+        order: {
+          labels: ['Red', 'Blue', 'Yellow'],
+          data: [300, 50, 100],
+          options: {
+            title: {
+              text: 'Proporsi banyaknya pesanan',
+            },
+          },
+        },
+        amount: {
+          labels: ['Red', 'Blue', 'Yellow'],
+          data: [300, 50, 100],
+          options: {
+            title: {
+              text: 'Proporsi total harga',
+            },
+          },
+        },
+      },
+      bar: {
+        order: {
+          labels: ['Red', 'Blue', 'Yellow'],
+          data: [300, 50, 100],
+          options: {
+            title: {
+              text: 'Jumlah banyaknya pesanan',
+            },
+          },
+        },
+        amount: {
+          labels: ['Red', 'Blue', 'Yellow'],
+          data: [300, 50, 100],
+          options: {
+            title: {
+              text: 'Jumlah total harga pesanan',
+            },
+          },
+        },
+      },
+      line: {
+        order: {
+          labels: ['10.00', '11.00', '12.00', '13.00', '14.00', '15.00', '16.00', '17.00', '18.00'],
+          series: ['Jakarta', 'Bandung', 'Surabaya'],
+          data: [
+            [65, 59, 80, 81, 56, 55, 40, 14, 14],
+            [28, 48, 40, 19, 86, 27, 90, 80, 80],
+            [18, 38, 30, 29, 76, 37, 80, 70, 80],
+          ],
+          datasetOverride: [{
+            fill: false,
+          },{
+            fill: false,
+          },{
+            fill: false,
+          }],
+          options: {
+            title: {
+              text: 'Banyaknya pesanan',
+            },
+          },
+        },
+        amount: {
+          labels: ['10.00', '11.00', '12.00', '13.00', '14.00', '15.00', '16.00', '17.00', '18.00'],
+          series: ['Jakarta', 'Bandung', 'Surabaya'],
+          data: [
+            [65, 59, 80, 81, 56, 55, 40, 14, 14],
+            [28, 48, 40, 19, 86, 27, 90, 80, 80],
+            [18, 38, 30, 29, 76, 37, 80, 70, 80],
+          ],
+          datasetOverride: [{
+            fill: false,
+          },{
+            fill: false,
+          },{
+            fill: false,
+          }],
+          options: {
+            title: {
+              text: 'Total harga',
+            },
+          },
+        },
+      },
+    };
+
+    /* maps */
+
+    var onEvent = function(trigger) {
+      vm.labelSubmit = 'Loading...';
+      console.log(trigger+' fired');
+    }
+
+    var lastRequest = 0;
+    var payload = {
+      lat_from: -6.2303955,
+      long_from: 106.8480445,
+      lat_to: -6.0303955,
+      long_to: 107.0480445,
+      time_from: 10,
+      time_to: 18,
+      n_items: sqrtItemCount * sqrtItemCount,
+    };
+    var onChangeBounds = function() {
+      vm.errorMessage = '';
+
+      var bounds = maps.map.getBounds();
+      payload.lat_from = bounds.f.f;
+      payload.long_from = bounds.b.b;
+      payload.lat_to = bounds.f.b;
+      payload.long_to = bounds.b.f;
+      payload.time_from = vm.time.from;
+      payload.time_to = vm.time.to;
+
+      var myRequest = (new Date).getTime();
+      lastRequest = myRequest;
+      console.log('requesting...', myRequest);
+
+      $http.post(apiUrl, payload).then(function(res) {
+        if (res.status == 200) {
+          if (myRequest == lastRequest) {
+            maps.draw(res.data.data.points, sqrtItemCount);
+            vm.labelSubmit = 'Submit';
+          }
         }
+      }, function(res) {
+        console.log(res);
+        vm.errorMessage = 'Error fetching data: '+res.status+' '+res.statusText;
+        vm.labelSubmit = 'Submit';
+      });
+    };
+
+    var maps = new Maps(onEvent, onChangeBounds, sqrtItemCount);
+
+    /* methods */
+
+    vm.goto = function(city) {
+      maps.goto(city);
+    };
+
+    vm.refresh = function() {
+      onEvent('submit');
+      onChangeBounds();
+    };
+
+    function updateChartData(newValue, oldValue) {
+      if (newValue != oldValue) {
+        console.log('change chart data', newValue, oldValue);
       }
-    }, function(res) {
-      console.log(res);
-      vm.errorMessage = 'Error fetching data: '+res.status+' '+res.statusText;
-      vm.labelSubmit = 'Submit';
-    });
-  };
+    }
 
-  var maps = new Maps(onEvent, onChangeBounds, sqrtItemCount);
+    $scope.$watch('vm.status', updateChartData);
+    $scope.$watch('vm.city', updateChartData);
+    updateChartData(true, false);
 
-  /* methods */
+    /* init */
 
-  vm.refresh = function() {
-    onEvent('submit');
-    onChangeBounds();
-  };
-
-  /* init */
-
-  vm.location.onChange();
-  vm.day.onChange();
-  document.body.style.visibility = 'visible';
-}]);
+    document.body.style.visibility = 'visible';
+  }]);
+})();
--- js/maps.js
+++ js/maps.js
@@ -1,12 +1,44 @@
 function Maps(onEvent, onChangeBounds, sqrtItemCount) {
   var self = this;
 
-  var map = new google.maps.Map(d3.select('#map').node(), {
+  var cities = {
+    jakarta: {
+      lat: -6.2303955,
+      long: 106.8480445,
+    },
+    bandung: {
+      lat: -6.9175,
+      long: 107.6191,
+    },
+    surabaya: {
+      lat: -7.2575,
+      long: 112.7521,
+    },
+    denpasar: {
+      lat: -8.6705,
+      long: 115.2126,
+    },
+    makassar: {
+      lat: -5.1477,
+      long: 119.4327,
+    },
+    palembang: {
+      lat: -2.9761,
+      long: 104.7754,
+    },
+  };
+
+  var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 11,
     center: new google.maps.LatLng(-6.2303955, 106.8480445),
     mapTypeControlOptions: {
       mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'gogogojek'],
     },
+    mapTypeControl: false,
+    scaleControl: false,
+    streetViewControl: false,
+    rotateControl: false,
+    fullscreenControl: false,
   });
   self.map = map;
 
@@ -65,5 +97,13 @@ function Maps(onEvent, onChangeBounds, sqrtItemCount) {
       radius: Math.round(Math.max($('#map').width(), $('#map').height()) / (sqrtItemCount / 1.4)),
     });
     lastHeatmap = currentHeatmap;
-  }
+  };
+
+  self.goto = function(city) {
+    var loc = cities[city];
+    if (loc) {
+      var center = new google.maps.LatLng(loc.lat, loc.long);
+      map.panTo(center);
+    }
+  };
 }
